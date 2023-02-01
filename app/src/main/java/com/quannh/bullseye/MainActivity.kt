d4d29379e8d5812a52946cbd993b3ae4974e60da
quannh287 <quannh2871@gmail.com>
2023-02-01 22:56:25 +0700

    Init first Kotlin Android project

---
+++ app/src/main/java/com/quannh/bullseye/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.quannh.bullseye
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
