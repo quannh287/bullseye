1e2032f2f13a43ad25b5c82094bf7e2021568c18
quannh287 <quannh2871@gmail.com>
2023-02-02 22:03:43 +0700

    Interaction in Android

--- app/src/main/java/com/quannh/bullseye/MainActivity.kt
+++ app/src/main/java/com/quannh/bullseye/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.quannh.bullseye
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.quannh.bullseye.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private var sliderValue = 0
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val randomNum = Random.nextInt(0, 100)
+        binding.targetValue.text = randomNum.toString()
+
+        binding.homeButton.setOnClickListener {
+            showResult()
+        }
+
+        binding.seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                sliderValue = progress
+                binding.targetValue.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
+    }
+
+    private fun showResult() {
+        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogMessage = "The slider \'s value is $sliderValue"
+
+        val builder = AlertDialog.Builder(this)
+
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.setPositiveButton(R.string.home_btn) { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        builder.create().show()
     }
 }
