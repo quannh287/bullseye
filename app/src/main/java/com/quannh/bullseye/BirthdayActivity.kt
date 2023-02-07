d4646e63f0cc920bb1d2bcfa5586a70c01c9c81c
quannh287 <quannh2871@gmail.com>
2023-02-07 15:51:17 +0700

    Update Random Dice Roller Logic

--- app/src/main/java/com/quannh/bullseye/BirthdayActivity.kt
+++ app/src/main/java/com/quannh/bullseye/BirthdayActivity.kt
@@ -7,6 +7,5 @@ class BirthdayActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday)
-        supportActionBar?.hide()
     }
 }
--- app/src/main/java/com/quannh/bullseye/RandomDiceActivity.kt
+++ app/src/main/java/com/quannh/bullseye/RandomDiceActivity.kt
@@ -2,11 +2,28 @@ package com.quannh.bullseye
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.quannh.bullseye.databinding.ActivityRandomDiceBinding
 
 class RandomDiceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRandomDiceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_random_dice)
+        binding = ActivityRandomDiceBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val randomDiceValue = binding.randomDiceValue
+        val firstDice = Dice(6)
+        binding.btnRoll.setOnClickListener{
+            randomDiceValue.text = firstDice.roll().toString()
+        }
+    }
+}
+
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
